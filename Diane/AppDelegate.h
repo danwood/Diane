cacb027ba50e4db7cb62c8c7273bb53892bddfca
Dan Wood <danwood@users.noreply.github.com>
2018-07-05 12:32:06 -0700

    Initial Commit

---
+++ Diane/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Diane
+//
+//  Created by Dan Wood on 7/5/18.
+//  Copyright © 2018 Karelia Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Diane/DetailViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.h
+//  Diane
+//
+//  Created by Dan Wood on 7/5/18.
+//  Copyright © 2018 Karelia Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Diane+CoreDataModel.h"
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) Event *detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
